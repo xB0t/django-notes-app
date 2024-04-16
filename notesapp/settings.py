99ccddd58ac58509699315191b242b51b6d24be6
Yash Chaudhari <ychaudhari124@gmail.com>
2024-04-16 16:46:56 +0530

    Added CSRF TRUSTED ORIGINS

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -145,3 +145,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['https://notes.xb0t.xyz']
